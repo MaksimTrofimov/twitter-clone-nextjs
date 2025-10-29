f17ff833c21a0f5644edc7e2f57aab0c18400461
Maksim <maksim.t.kh@gmail.com>
2025-10-29 18:08:55 +0100

    add header component

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import { Logo } from "@/components/Logo";
+
+export function Header() {
+  return (
+    <header className="flex justify-center items-center h-[45px]">
+      <Logo />
+    </header>
+  );
+}
