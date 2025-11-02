cc3c06eeb3ebb6bbb05ee75e3dfb8c31dedf73a2
Maksim <maksim.t.kh@gmail.com>
2025-11-02 21:43:00 +0100

    import getTweets service in Home component to fetch tweets

--- app/tweet/page.js
+++ app/tweet/page.js
@@ -1,8 +1,10 @@
 import { Header } from '@/components/Header';
 import { MainHeader } from '@/components/MainHeader';
+import { getTweets } from '@/services/tweetService';
 
 export default async function Home() {
   const { headerLeft, headerCenter } = MainHeader();
+  const tweets = await getTweets();
 
   return (
     <>
