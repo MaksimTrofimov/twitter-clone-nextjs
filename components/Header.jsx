76c9ce8a682d38ba28d1d0c5db48f9f352d3902d
Maksim <maksim.t.kh@gmail.com>
2025-11-02 21:34:33 +0100

    refactor Header component to accept customizable left, center, and right props

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
-import { Logo } from "@/components/Logo";
-
-export function Header() {
+export function Header({ left = null, center = null, right = null }) {
   return (
-    <header className="flex justify-center items-center h-[45px]">
-      <Logo />
+    <header className="relative flex h-[45px] items-center">
+      <div className="absolute left-0 pl-4">{left}</div>
+      <div className="absolute left-1/2 -translate-x-1/2">{center}</div>
+      <div className="absolute right-0 pr-4">{right}</div>
     </header>
   );
 }
