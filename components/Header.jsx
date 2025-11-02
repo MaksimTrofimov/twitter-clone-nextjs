528dd068bf70982d77aa509cbc5c629dbdb2f07a
Maksim <maksim.t.kh@gmail.com>
2025-11-02 23:16:48 +0100

    add active tab functionality to Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,40 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+
 export function Header({ left = null, center = null, right = null }) {
+  const [activeTab, setActiveTab] = useState('forYou');
+  const forYouClass =
+    activeTab === 'forYou'
+      ? 'font-bold text-black border-b-2 border-blue-500'
+      : 'font-normal text-gray-500 border-b-2 border-transparent';
+  const followingClass =
+    activeTab === 'following'
+      ? 'font-bold text-black border-b-2 border-blue-500'
+      : 'font-normal text-gray-500 border-b-2 border-transparent';
+
   return (
-    <header className="relative flex h-[45px] items-center">
-      <div className="absolute left-0 pl-4">{left}</div>
-      <div className="absolute left-1/2 -translate-x-1/2">{center}</div>
-      <div className="absolute right-0 pr-4">{right}</div>
+    <header>
+      <div className="relative flex h-[45px] items-center">
+        <div className="absolute left-0 pl-4">{left}</div>
+        <div className="absolute left-1/2 -translate-x-1/2">{center}</div>
+        <div className="absolute right-0 pr-4">{right}</div>
+      </div>
+
+      <nav className="pt-4">
+        <ul className="flex items-center justify-center gap-22">
+          <li className={forYouClass} onClick={() => setActiveTab('forYou')}>
+            <Link href="/tweet">For you</Link>
+          </li>
+          <li
+            className={followingClass}
+            onClick={() => setActiveTab('following')}
+          >
+            <Link href="/tweet">Following</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
