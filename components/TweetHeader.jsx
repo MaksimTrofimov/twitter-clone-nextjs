a40ddbb6db74dc14c7411bd5e1248c927ca7bf76
Maksim <maksim.t.kh@gmail.com>
2025-11-13 23:31:26 +0100

    refactor(ui): change style headerCenter in TweetHeader component

--- components/TweetHeader.jsx
+++ components/TweetHeader.jsx
@@ -3,7 +3,7 @@ import BackButton from './BackButton';
 export function TweetHeader({ tweet }) {
   const headerLeft = <BackButton href="/tweet" />;
 
-  const headerCenter = <span className="text-lg">Tweet</span>;
+  const headerCenter = <span className="text-lg font-bold">Tweet</span>;
 
   return { headerLeft, headerCenter };
 }
