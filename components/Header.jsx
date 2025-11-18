a12655aea4ec66ee80d91c8a79396d9f5bea3f65
Maksim <maksim.t.kh@gmail.com>
2025-11-18 20:54:41 +0100

    refactor(header): remove NavComponent prop and related rendering from Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,4 @@
-export function Header({
-  left = null,
-  center = null,
-  right = null,
-  NavComponent = null,
-}) {
+export function Header({ left = null, center = null, right = null }) {
   return (
     <header className="mt-2">
       <div className="relative flex h-[45px] items-center">
@@ -11,8 +6,6 @@ export function Header({
         <div className="absolute left-1/2 -translate-x-1/2">{center}</div>
         <div className="absolute right-0 pr-4">{right}</div>
       </div>
-
-      <nav className="pt-4">{NavComponent && <NavComponent />}</nav>
     </header>
   );
 }
