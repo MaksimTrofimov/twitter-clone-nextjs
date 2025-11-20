e509f45af64cea499a9820bf199f540aa19d668c
Maksim <maksim.t.kh@gmail.com>
2025-11-20 08:18:47 +0100

    add migration

---
+++ migrations/20251119174355-create_users_collection.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    await db.createCollection('users');
+    await db.collection('users').createIndex({ email: 1 }, { unique: true });
+    await db.collection('users').createIndex({ username: 1 }, { unique: true });
+
+    await db.collection('users').insertOne({
+      username: ' test',
+      email: 'test@example.com',
+      passwordHash: '$2b$10$replace_with_hashed_password',
+      name: 'Test',
+      avatarUrl: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      followersCount: 0,
+      followingCount: 0,
+      isVerified: true,
+    });
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+};
