da66884a03b82ed10414303392639a241ac3b3a6
Maksim <maksim.t.kh@gmail.com>
2025-12-05 09:33:25 +0100

    feat(user): add dateOfBirth field to userSchema

--- src/models/User.js
+++ src/models/User.js
@@ -41,6 +41,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
   },
   {
     timestamps: true,
